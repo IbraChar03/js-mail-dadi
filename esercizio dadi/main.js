64a9f806b8b344c6b5d8a3f2a43e5cdba2537f2c
Ibrahim <ibrachar68@gmail.com>
2022-11-10 16:54:34 +0100

    creazione button

--- esercizio dadi/main.js
+++ esercizio dadi/main.js
@@ -1,20 +1,27 @@
-// numero random giocatore
-let numPlayer=Math.floor(Math.random()*6)+1;
-console.log(numPlayer);
-//numero random pc
-let numPc=Math.floor(Math.random()*6)+1;
-console.log(numPc);
+let tasto=document.querySelector("button");
+
+tasto.addEventListener("click",
+  function(){
+    let numPlayer=Math.floor(Math.random()*6)+1;
+    console.log(numPlayer);
+
+    let numPc=Math.floor(Math.random()*6)+1;
+    console.log(numPc);
+
 
 //condizione
-if(numPlayer > numPc){
+    if(numPlayer > numPc){
     let stampa= document.getElementById("title");
     stampa.innerHTML="Ha vinto il player";
-}
-else if(numPlayer < numPc){
+    }
+    else if(numPlayer < numPc){
     let stampa= document.getElementById("title");
     stampa.innerHTML="Ha vinto il pc";
-}
-else if(numPlayer === numPc){
+    }
+    else if(numPlayer === numPc){
     let stampa= document.getElementById("title");
     stampa.innerHTML="Avete pareggiato";
-}
+    }
+  }
+
+);
