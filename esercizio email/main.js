55e3bc8bc81f7d9e61dd7899874fb7a97afa87c4
Ibrahim <ibrachar68@gmail.com>
2022-11-10 13:18:50 +0100

    parte js esercizio email

--- esercizio email/main.js
+++ esercizio email/main.js
@@ -2,7 +2,7 @@ const email=["luca@gmail.com", "mario@gmail.com"];
 let emailnuova= prompt("inserisci la tua email:");
 
 for(let i=0; i< email.length;i++){
-    if(email === emailnuova){
+    if(email[i] === emailnuova){
         let stampa=document.getElementById("title");
         stampa.innerHTML="sei nella lista degli invitati";
     }
